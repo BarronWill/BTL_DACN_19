a935c84a27e4fa64abaa50079ba61e762682c7df
BarronWill <viet22082003@gmail.com>
2024-08-29 21:53:08 +0700

    Update CF file

--- Collaborative_Filtering.py
+++ Collaborative_Filtering.py
@@ -3,13 +3,13 @@ import pandas as pd
 from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
 
-data = pd.read_csv('Data/new_book_Rating_095.csv')
+data = pd.read_csv('Data/new_book_Rating_095.csv').drop_duplicates(['User_id', 'Title'])
 print(data.head())
 
 # Check null
 data.isnull().sum()
 
-book_features_df = data.pivot_table(index='User_id', columns='Title', values='review/score').fillna(0)
+book_features_df = data.pivot_table(index='User_id', columns='Title', values='review/score').fillna(0)[:100]
 
 
 book_features_df_matrix = csr_matrix(book_features_df.values)
@@ -19,14 +19,30 @@ model_knn = NearestNeighbors(metric='cosine', algorithm='brute')
 model_knn.fit(book_features_df_matrix)
 
 query_index = np.random.choice(book_features_df.shape[0])
-print(query_index)
-distances, indices = model_knn.kneighbors(book_features_df.iloc[query_index,:].values.reshape(1, -1), n_neighbors = 6)
+distances, indices = model_knn.kneighbors(book_features_df.iloc[query_index,:].values.reshape(1, -1), n_neighbors = 25)
 
+active_user_ratings = book_features_df.iloc[query_index,:]
+unrated_items = active_user_ratings[active_user_ratings == 0].index
 
-active_user = book_features_df.iloc[query_index,:]
-active_user.shape
+predicted_ratings = {}
+
+# Dự đoán đánh giá cho mỗi cuốn sách chưa đọc
+for item_index in unrated_items:
+    ratings_sum = 0
+    count = 0
+    for neighbor_index in indices[0]:
+        neighbor_ratings = book_features_df.iloc[neighbor_index]
+        if neighbor_ratings[item_index] != 0:  # Chỉ xem xét người dùng đã đánh giá
+            ratings_sum += neighbor_ratings[item_index]
+            count += 1
+
+    if count == 0:
+        predicted_ratings[item_index] = 0
+    else:
+        predicted_ratings[item_index] = ratings_sum / count
 
 # Recommend
-similar_user_ratings = book_features_df.iloc[indices.flatten()[1]]
-items_list = similar_user_ratings[(active_user > 0) & (similar_user_ratings >= 4)]
-print("Sách được đề xuất là: ", items_list.index.tolist())
+sorted_predictions = sorted(predicted_ratings.items(), key=lambda x: -x[1])
+recommendations = [(item_index, rating) for item_index, rating in sorted_predictions[:6]]
+
+print(recommendations)
